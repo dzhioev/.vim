22ab205fc3261d5378b74ba554a0bab1d351790a
Pavel Sergeev <dzhioev@chromium.org>
2013-11-01 19:14:56 +0400

    Corrected command-t submodule.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive
 DIRNAME=$(basename $(pwd))
 cd .. && ln -s $DIRNAME/.vimrc && cd -
 # Command-T
-cd bundle/command-t/ruby/command-t && ruby extconf.rb && make && cd -
+cd bundle/Command-T/ruby/command-t && ruby extconf.rb && make && cd -
 # YouCompleteMe
 cd bundle/YouCompleteMe && ./install.sh --clang-completer && cd -
 vim +Helptags +q

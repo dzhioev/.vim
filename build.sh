d8d5a166b7b882886d6aa11b7c2049d482096657
Pavel Sergeev <dzhioev@chromium.org>
2014-04-11 14:50:34 +0400

    Added vim-fugitive plugin.

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # Command-T
 cd bundle/Command-T/ruby/command-t && ruby extconf.rb && make && cd -
+
 # YouCompleteMe
 cd bundle/YouCompleteMe && ./install.sh --clang-completer && cd -
 vim +Helptags +q

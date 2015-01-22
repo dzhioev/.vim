afa9f822d0db9cc279f89df4fc4e5be9d0591aaa
Pavel Sergeev <dzhioev@chromium.org>
2015-01-22 13:33:01 +0300

    update.sh updated.

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 
 git submodule foreach git fetch
 git submodule foreach git checkout origin/HEAD
-git submodule foreach git submodule update --init
+git submodule foreach git submodule update --init --recursive
 ./build.sh
